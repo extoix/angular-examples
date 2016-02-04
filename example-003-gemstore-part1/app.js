e3d205db9bdbbaecc683bd1d6de8ada50bb3765f
extoix <extoix@usres.noreply.github.com>
2016-02-03 22:01:18 -0600

    Added .gitignore for idea files Added example-001-hello-world Added example-002-todo Added example-003-gemstore-part1

---
+++ example-003-gemstore-part1/app.js
@@ -0,0 +1,32 @@
+(function() {
+    var app = angular.module('store', []);
+
+    app.controller('StoreController', function() {
+       this.products = gems;
+    });
+
+    var gems = [
+        {
+            name: 'Dodecahedron',
+            price: 2.95,
+            description: '. . .',
+            canPurchase: true,
+            soldOut: false
+        },
+        {
+            name: 'Pentagonal Gem',
+            price: 5.95,
+            description: '. . .',
+            canPurchase: true,
+            soldOut: false
+        },
+        {
+            name: 'Zircon',
+            price: 3.95,
+            description: '. . .',
+            canPurchase: true,
+            soldOut: false
+        }
+    ];
+
+})();
